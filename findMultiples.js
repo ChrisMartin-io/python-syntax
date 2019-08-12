d50af9867c22b6434f0098cbde4c110cbc4c04a3
stephenau93 <stephenau93@gmail.com>
2019-08-12 11:50:54 -0700

    added findMultiples.js

---
+++ findMultiples.js
@@ -0,0 +1,22 @@
+function findMultiples(arr, count) {
+  let freqObj = freqCounter(arr);
+  let retArr = [];
+  for (let key in freqObj) {
+    if (freqObj[key] >= count) {
+      retArr.push(+key);
+    }
+  }
+  return retArr;
+}
+
+function freqCounter(arr) {
+  let obj = {};
+  for (let i = 0; i < arr.length; i++) {
+    if (obj[arr[i]] === undefined) {
+      obj[arr[i]] = 1;
+    } else {
+      obj[arr[i]] += 1;
+    }
+  } 
+  return obj;
+}
